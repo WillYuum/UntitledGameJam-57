2c2e5a2bc3b2497ed8d623b5224f5c002ea75636
WilliamAB <liams_1998@hotmail.com>
2021-10-23 19:48:24 +0300

    Create chaser FOW logic

---
+++ Assets/Scripts/Chaser/Chaser.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Chaser
+{
+    public class Chaser : MonoBehaviour
+    {
+
+
+    }
+}
---
+++ Assets/Scripts/Chaser/FOW.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Chaser
+{
+    public class FOW : MonoBehaviour
+    {
+        public float viewRadius;
+        [Range(0, 360)]
+        public float viewAngle;
+
+        public LayerMask targetMask;
+        public LayerMask obstacleMask;
+
+        [HideInInspector]
+        public List<Transform> visibleTargets = new List<Transform>();
+
+        public void EnableFOW()
+        {
+            StartCoroutine(nameof(FindTargetsWithDelay), .2f);
+        }
+
+        public void DisableFOW()
+        {
+            StopCoroutine(nameof(FindTargetsWithDelay));
+        }
+
+
+        IEnumerator FindTargetsWithDelay(float delay)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(delay);
+                FindVisibleTargets();
+            }
+        }
+
+        private void FindVisibleTargets()
+        {
+            visibleTargets.Clear();
+            Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+            for (int i = 0; i < targetsInViewRadius.Length; i++)
+            {
+                Transform target = targetsInViewRadius[i].transform;
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+                {
+                    float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                    if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                    {
+                        visibleTargets.Add(target);
+                    }
+                }
+            }
+        }
+
+
+        public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+        {
+            if (!angleIsGlobal)
+            {
+                angleInDegrees += transform.eulerAngles.y;
+            }
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad), 0);
+        }
+    }
+}
