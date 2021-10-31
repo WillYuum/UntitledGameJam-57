be066bd0001f65d8cf8239fc396978c6251ad895
WilliamAB <liams_1998@hotmail.com>
2021-11-01 01:33:15 +0200

    Create sequence move logic

---
+++ Assets/Scripts/RouteController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+using DG.Tweening;
+
+public class RouteController : MonoBehaviour
+{
+    [SerializeField] private float moveDuration = 1.0f;
+    [SerializeField] private PsuedoRandArray<Transform> positionToMoveTo;
+    [SerializeField] private DelayController waitToMoveNextPoint;
+
+    private Transform currentPointMovingTo;
+
+    private bool isWaiting = false;
+    void Awake()
+    {
+        if (positionToMoveTo.items.Length == 0)
+        {
+            enabled = false;
+        }
+        else
+        {
+            positionToMoveTo.ToggleShuffle(false);
+            isWaiting = true;
+            currentPointMovingTo = positionToMoveTo.PickNext();
+        }
+    }
+
+
+    void Update()
+    {
+        if (isWaiting)
+        {
+            print("IsWaitin");
+            waitToMoveNextPoint.IncrementTimer(out bool isFinishedDelay);
+            if (isFinishedDelay)
+            {
+                GoToNextPoint();
+                waitToMoveNextPoint.ResetTimer();
+            }
+        }
+    }
+
+    private void GoToNextPoint()
+    {
+        isWaiting = false;
+
+        Transform newPointToMoveTo = positionToMoveTo.PickNext();
+        transform
+        .DOMove(newPointToMoveTo.position, moveDuration)
+        .SetEase(Ease.Linear)
+        .OnComplete(OnReachedPoint);
+
+        currentPointMovingTo = newPointToMoveTo;
+    }
+
+    private void OnReachedPoint()
+    {
+        isWaiting = true;
+    }
+
+
+
+}
